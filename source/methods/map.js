5e943dc5e9770fabca7dfbe5215ec9f5517f8d4e
fedeghe <fedeghe@gmail.com>
2020-08-16 17:05:12 +0200

    small fix

--- source/methods/map.js
+++ source/methods/map.js
@@ -1,12 +1,9 @@
 function map(o, func) {
-    if (!core.in.isObjOrArr(o)) throw new Error(core.errors.INVALID_ARGUMENT_OBJECT_OR_ARRAY_EXPECTED)
-    var res = [];
+    if (!core.in.isObjOrArr(o)) throw new Error(core.errors.INVALID_ARGUMENT_OBJECT_OR_ARRAY_EXPECTED);
     func = func || function (obj){return obj;};
     if (core.in.isObj(o)) {
         return core.ut.objLoop(o, func)
     }
-    if (core.in.isArr(o)){
-        return core.ut.arrLoop(o, func)
-    }
-    return res;
+    // thus array
+    return core.ut.arrLoop(o, func)
 }
