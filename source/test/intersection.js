da701b5724473bc499939151fc4b1eedc930f06d
fedeghe <fedeghe@gmail.com>
2021-10-31 15:47:32 +0100

    v1.1.10

--- source/test/intersection.js
+++ source/test/intersection.js
@@ -13,8 +13,10 @@ describe('intersection', () => {
         )), JSON.stringify([1, 2, 3]));
 
         assert.strictEqual(JSON.stringify(ow.intersection(
-            [0, 2, 4, 5, 1, 4, 5, 'a', 'A', 33, 234, 3, 'c'], [0, 'a', 'A', 33, 'c'], [0, 'a', 'A', 234, 45, 'c']
-        )), JSON.stringify([0, 'a', 'A', 234, 'c']))
+            [0, 2, 4, 5, 1, 4, 5, 'a', 'A', 33, 234, 3, 'c'],
+            [0, 'a', 'A', 33, 'c'],
+            [0, 'a', 'A', 234, 45, 'c']
+        )), JSON.stringify([0, 'a', 'A', 'c']))
     });
     it('should work as expected also with strings', () =>
         assert.strictEqual(JSON.stringify(ow.intersection(
